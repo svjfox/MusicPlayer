49f7c3f5ea983ef756dbff7403018d520fd3d012
VitaliiS <svjfox@gmail.com>
2025-04-03 16:06:33 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicPlayer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
